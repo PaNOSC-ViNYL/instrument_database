7b2f4ae52348be10e124c94ed7326a7e1fd6d97c
Shervin Nourbakhsh <nourbakhsh@ill.fr>
2024-02-28 09:14:29 +0100

    fixing the virtual source for monochromators

--- institutes/ILL/sources/HEAD/mcstas/Monochromator_source.py
+++ institutes/ILL/sources/HEAD/mcstas/Monochromator_source.py
@@ -11,12 +11,12 @@ def HCS_source(mcstas_instrument: McCode_instr) -> Component:
 
     The source should be added after the origin component is declared
     """
-    Ei = mcstas_instrument.add_parameter(
-        "double", "Ei", comment="Initial neutron energy", value=0, unit="meV"
-    )
-    dE = mcstas_instrument.add_parameter(
-        "double", "dE", comment="Initial neutron energy dispertion", value=0, unit="meV"
-    )
+    # Ei = mcstas_instrument.add_parameter(
+    #    "double", "Ei", comment="Initial neutron energy", value=0, unit="meV"
+    # )
+    # dE = mcstas_instrument.add_parameter(
+    #    "double", "dE", comment="Initial neutron energy dispertion", value=0, unit="meV"
+    # )
 
     #    slambda = mcstas_instrument.add_parameter(
     #        "double", "lambda", value=0, unit="angstrom"
@@ -26,7 +26,9 @@ def HCS_source(mcstas_instrument: McCode_instr) -> Component:
     )
 
     last_component = mcstas_instrument.get_last_component()
-    HCS = mcstas_instrument.add_component("HCS", "Source_simple")
+    HCS = mcstas_instrument.add_component(
+        "HCS", "Source_simple", AT=0, RELATIVE=last_component
+    )
     HCS.radius = 0.21 / 2
     #    HCS.dist = 2.155
     #    HCS.focus_xw = 0.170
@@ -37,24 +39,24 @@ def HCS_source(mcstas_instrument: McCode_instr) -> Component:
     HCS.E0 = "Ei"
     HCS.dE = "dE"
     HCS.flux = 1e13
-    #    HCS.lambda0 = "lambda"
-    #    HCS.dlambda = "dlambda"
+    # HCS.lambda0 = "lambda"
+    # HCS.dlambda = "dlambda"
     #    HCS.Lmin = 0
     #    HCS.Lmax = 0
 
     HCS.gauss = 1
-    HCS.set_AT(["0", " 0", " 0"], RELATIVE=last_component)
 
-    HCS.target_index = 1
+    # HCS.target_index = 1
 
     # override the value of lambda by the value of the angle
     mcstas_instrument.add_declare_var("double", "lambda")
+    mcstas_instrument.add_declare_var("double", "Ei")
+    mcstas_instrument.add_declare_var("double", "dE")
 
     mcstas_instrument.append_initialize(
         "lambda =2*sin(0.5*a2*DEG2RAD)*monochromator_d;"
     )
-    mcstas_instrument.append_initialize('printf("lambda: %.2f\\n", lambda);')
-    mcstas_instrument.append_initialize("dlambda=0.01*lambda;")
+    mcstas_instrument.append_initialize("dlambda = dlambda * lambda;")
     # the following python string is an alternative way of doing:
     mcstas_instrument.add_declare_var("double", "energy")
     mcstas_instrument.append_initialize("Ei = 81.80421/(lambda*lambda);")
