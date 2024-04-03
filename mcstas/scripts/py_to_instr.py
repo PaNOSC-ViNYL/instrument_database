dc673d1e59983496b063fdb5f8af8f2b3464c9ff
Shervin Nourbakhsh <4571685+shervin86@users.noreply.github.com>
2024-04-03 10:58:10 +0200

    removing attenuator and parameter, add test setup

--- mcstas/scripts/py_to_instr.py
+++ mcstas/scripts/py_to_instr.py
@@ -15,12 +15,23 @@ else:
 
 sample_name = sys.argv[5]
 sample_environment_name = sys.argv[6]
+sample_holder_material = sys.argv[7]
 
-repo = API.Repository(local_repo=os.path.dirname(__file__) + "/../../")
+repo = API.Repository(local_repo=os.path.dirname(__file__) + "/../..")
 
 myinstrument = repo.load(institute, instrument, version, software, flavour, dep=False)
 
 myinstrument.set_sample_by_name(sample_name)
+if sample_holder_material != "None":
+    myinstrument.sample_holder(
+        material=sample_holder_material,
+        shape="box",
+        w=0.02,
+        h=0.03,
+        d=0.0135,
+        th=0.00125,
+    )
+
 myinstrument.set_sample_environment_by_name(sample_environment_name)
 
 calc_with_sample = myinstrument._calculator_with_sample
