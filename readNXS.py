49a8efc7c54d0dfb23f3e4a5919a8b20ad32af35
Shervin Nourbakhsh <4571685+shervin86@users.noreply.github.com>
2024-04-03 10:58:10 +0200

    sample shape and sample holder methods improved

--- readNXS.py
+++ readNXS.py
@@ -2,24 +2,119 @@
 Reads a D11 NeXus file ...
 """
 
+
+def center_of_mass(data, nx_min, nx_max, ny_min, ny_max):
+    cx = 0.0
+    cy = 0.0
+    nx = 0.0
+    ny = 0.0
+    for i in range(nx_min, nx_max + 1):
+        cx += np.sum(data[i, :]) * i
+        nx += np.sum(data[i, :])
+    for i in range(ny_min, ny_max + 1):
+        cy += np.sum(data[:, i]) * i
+        ny += np.sum(data[:, i])
+    return cx / nx + 1, cy / ny + 1
+
+
 import sys
 import os
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+from mcstasscript.interface.functions import load_metadata, load_monitor
 
 
-file = sys.argv[1]
-print("Reading file" + file)
+file_data = sys.argv[1]
+simulation_dir = sys.argv[2]
+
+print("Reading file: " + file_data)
 
 
 # Read NeXus
-f = h5py.File(file, "r")
-central_detector = f["entry0"]["D11"]["Detector 1"]["data"]
-print(central_detector.shape)
-plt.imshow(central_detector, aspect="auto", cmap="seismic", origin="lower")
-f.close()
+f = h5py.File(file_data, "r")
+central_detector_data = f["entry0"]["D11"]["Detector 1"]["data"][:, :, 0]
+left_detector_data = f["entry0"]["D11"]["Detector 2"]["data"][:, :, 0]
+right_detector_data = f["entry0"]["D11"]["Detector 3"]["data"][:, :, 0]
 
+detectors_data = {
+    "left": left_detector_data,
+    "central": central_detector_data,
+    "right": right_detector_data,
+}
+
+metadata_list = load_metadata(simulation_dir)
+print(left_detector_data.shape, central_detector_data.shape, right_detector_data.shape)
+# print(metadata_list)
+
+detectors_simulation = {}
+for metadata in metadata_list:
+    if metadata.component_name in [
+        "detector_left",
+        "detector_right",
+        "detector_central",
+    ]:
+        monitor = load_monitor(metadata, simulation_dir)
+        if monitor.name == "detector_left":
+            detectors_simulation["left"] = monitor
+        elif monitor.name == "detector_right":
+            detectors_simulation["right"] = monitor
+        elif monitor.name == "detector_central":
+            detectors_simulation["central"] = monitor
+        # print("### MONITOR:")
+        # help(monitor)
+
+print(
+    detectors_simulation["left"].Intensity.transpose().shape,
+    detectors_simulation["central"].Intensity.transpose().shape,
+    detectors_simulation["right"].Intensity.transpose().shape,
+)
+ratio = {}
+ratio["left"] = (
+    detectors_simulation["left"].Intensity.transpose() + detectors_data["left"]
+)
+# print(ratio["left"])
+# help(detectors_simulation["right"].Intensity)
+# print(metadata_list["detector_right"])
+# plt.show()
+
+
+fig, axs = plt.subplots(2, 3)
+fig.suptitle("Vertically stacked subplots")
+axs[0][0].imshow(
+    detectors_data["left"].transpose(), aspect="auto", cmap="seismic", origin="lower"
+)
+axs[0][1].imshow(
+    detectors_data["central"].transpose(), aspect="auto", cmap="seismic", origin="lower"
+)
+axs[0][2].imshow(
+    detectors_data["right"].transpose(), aspect="auto", cmap="seismic", origin="lower"
+)
+
+axs[1][0].imshow(
+    detectors_simulation["left"].Intensity,
+    aspect="auto",
+    cmap="seismic",
+    origin="lower",
+)
+axs[1][1].imshow(
+    detectors_simulation["central"].Intensity,
+    aspect="auto",
+    cmap="seismic",
+    origin="lower",
+)
+axs[1][2].imshow(
+    detectors_simulation["right"].Intensity,
+    aspect="auto",
+    cmap="seismic",
+    origin="lower",
+)
+# cbar = fig.colorbar(detectors_simulation["central"])
+# cbar.set_label("ZLabel", loc="top")
+# cax = plt.axes([0.85, 0.1, 0.075, 0.8])
+# plt.colorbar(cax=cax)
+plt.show()
+f.close()
 sys.exit(0)
 print("NeXus data shapes:")
 print(data1.shape, data2.shape, data3.shape, data4.shape, data5.shape)
