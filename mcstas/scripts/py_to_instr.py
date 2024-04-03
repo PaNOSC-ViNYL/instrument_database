b1da9871b60889059bd7fd7547c1683a461f1feb
Shervin Nourbakhsh <4571685+shervin86@users.noreply.github.com>
2024-04-03 10:58:11 +0200

    argparse of options, output dir option added

--- mcstas/scripts/py_to_instr.py
+++ mcstas/scripts/py_to_instr.py
@@ -3,28 +3,36 @@ import os
 from instrumentdatabaseapi import instrumentdatabaseapi as API
 from mcstasscript.interface import instr
 
-institute = sys.argv[1]
-instrument = sys.argv[2]
-version = sys.argv[3]
-software = "mcstas"
-print(sys.argv)
-if len(sys.argv) >= 5:
-    flavour = sys.argv[4]
-else:
-    flavour = ""
+import argparse
 
-sample_name = sys.argv[5]
-sample_environment_name = sys.argv[6]
-sample_holder_material = sys.argv[7]
+parser = argparse.ArgumentParser(description="")
+parser.add_argument("--outdir", nargs="?", help="output directory", default=".")
+parser.add_argument("institute", help="name of the institute")
+parser.add_argument("instrument", help="name of the instrument")
+parser.add_argument("version", help="version of the instrument")
+parser.add_argument("software", help="simulation software used by the description")
+parser.add_argument("flavour", help="flavour of the description")
+parser.add_argument("sample_name", help="name of the sample")
+parser.add_argument("sample_environment_name", help="name of the sample environment")
+parser.add_argument("sample_holder_material", help="sample holder material")
+args = parser.parse_args()
+
 
 repo = API.Repository(local_repo=os.path.dirname(__file__) + "/../..")
 
-myinstrument = repo.load(institute, instrument, version, software, flavour, dep=False)
+myinstrument = repo.load(
+    args.institute,
+    args.instrument,
+    args.version,
+    args.software,
+    args.flavour,
+    dep=False,
+)
 
-myinstrument.set_sample_by_name(sample_name)
-if sample_holder_material != "None":
+myinstrument.set_sample_by_name(args.sample_name)
+if args.sample_holder_material != "None":
     myinstrument.sample_holder(
-        material=sample_holder_material,
+        material=args.sample_holder_material,
         shape="box",
         w=0.02,
         h=0.03,
@@ -32,23 +40,35 @@ if sample_holder_material != "None":
         th=0.00125,
     )
 
-myinstrument.set_sample_environment_by_name(sample_environment_name)
+myinstrument.set_sample_environment_by_name(args.sample_environment_name)
 
 calc_with_sample = myinstrument._calculator_with_sample
 
 instrfiles = []
+import os
+
+os.makedirs(args.outdir, exist_ok=True)
+os.chdir(args.outdir)
 for calcname in myinstrument.calculators:
     calc = myinstrument.calculators[calcname]
     # calc.show_components()
     if calcname == calc_with_sample.name:
         calc.name = (
-            calcname + "_" + flavour + "_" + sample_name + "_" + sample_environment_name
+            calcname
+            + "_"
+            + args.flavour
+            + "_"
+            + args.sample_name
+            + "_"
+            + args.sample_environment_name
         )
     else:
-        calc.name = calcname + "_" + flavour
+        calc.name = calcname + "_" + args.flavour
 
     if isinstance(calc, instr.McStas_instr):
-        instrfiles.append(calc.name)
-        calc.write_full_instrument()
 
+        calc.write_full_instrument()
+        newname = args.instrument + "_" + calc.name + ".instr"
+        os.rename(calc.name + ".instr", newname)
+        instrfiles.append(newname)
 print(instrfiles)
