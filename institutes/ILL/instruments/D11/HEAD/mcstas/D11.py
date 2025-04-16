89b6aa31c64ce482d9d1379683d4286cce9138bd
Shervin Nourbakhsh <4571685+shervin86@users.noreply.github.com>
2025-04-16 14:13:23 +0200

    restore higher fluxes

--- institutes/ILL/instruments/D11/HEAD/mcstas/D11.py
+++ institutes/ILL/instruments/D11/HEAD/mcstas/D11.py
@@ -361,8 +361,7 @@ class D11(McStasInstrumentBase):
                 focus_yh=0.05,
                 dist=2.3,
                 # flux=2.22e12,
-                # flux=16.0e12,
-                flux=1e11,
+                flux=16.0e12,
             )
         else:
             mysource = source.VCS_source(mycalculator)
